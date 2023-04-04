c12e0da9275d2a0e1d23517441b42d2780c34ddc
Gitrom-coder <roma.gerasimov2016@yandex.ru>
2023-04-04 15:59:12 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,111 @@
+
+
+export default function App() {
+
+
+  
+  return (<div className="FAQ">
+    <body>
+      <div class="container">
+        <h1 class="title">FAQ</h1>
+        <main class="accordion">
+          <div class="faq-img">
+            <img class="accordion-img" src="./illustration-woman-online-mobile.png" alt="no img"></img>
+          </div>
+          <div class="content-accordion">
+            <div class="question-answer">
+              <div class="question">
+                <h3 class="title-question">How many team members can I invite?</h3>
+                <button class="question-btn">
+                  <span class="up-icon">
+                  <i class="fa-solid fa-chevron-up"></i>
+                  </span>
+                  <span class="down-icon">
+                  <i class="fa-solid fa-chevron-down"></i>
+                  </span>
+                </button>
+              </div>
+              <div class="answer">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              </div>
+            </div>
+
+            <div class="question-answer">
+              <div class="question">
+                <h3 class="title-question">What is the maximum file upload size?</h3>
+                <button class="question-btn">
+                  <span class="up-icon">
+                  <i class="fa-solid fa-chevron-up"></i>
+                  </span>
+                  <span class="down-icon">
+                  <i class="fa-solid fa-chevron-down"></i>
+                  </span>
+                </button>
+              </div>
+              <div class="answer">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              </div>
+            </div>
+
+            <div class="question-answer">
+              <div class="question">
+                <h3 class="title-question">How do I reset my password?</h3>
+                <button class="question-btn">
+                  <span class="up-icon">
+                  <i class="fa-solid fa-chevron-up"></i>
+                  </span>
+                  <span class="down-icon">
+                  <i class="fa-solid fa-chevron-down"></i>
+                  </span>
+                </button>
+              </div>
+              <div class="answer">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              </div>
+            </div>
+
+            <div class="question-answer">
+              <div class="question">
+                <h3 class="title-question">Can I cancek my subscription?</h3>
+                <button class="question-btn">
+                  <span class="up-icon">
+                  <i class="fa-solid fa-chevron-up"></i>
+                  </span>
+                  <span class="down-icon">
+                  <i class="fa-solid fa-chevron-down"></i>
+                  </span>
+                </button>
+              </div>
+              <div class="answer">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              </div>
+            </div>
+
+            <div class="question-answer">
+              <div class="question">
+                <h3 class="title-question">Do you provide addional support?</h3>
+                <button class="question-btn">
+                  <span class="up-icon">
+                  <i class="fa-solid fa-chevron-up"></i>
+                  </span>
+                  <span class="down-icon">
+                  <i class="fa-solid fa-chevron-down"></i>
+                  </span>
+                </button>
+              </div>
+              <div class="answer">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              </div>
+            </div>
+
+          </div>
+        </main>
+      </div>
+
+    </body>
+  </div>
+)}
+
+
+
+
